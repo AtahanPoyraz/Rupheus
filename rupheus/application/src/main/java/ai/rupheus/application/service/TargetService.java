9cc994cf65d6a983eaa639726c0b9dd9ce90b34e
AtahanPoyraz <atahanpoyraz@gmail.com>
2025-12-06 21:12:24 +0300

    Updated endpoints

--- rupheus/application/src/main/java/ai/rupheus/application/service/TargetService.java
+++ rupheus/application/src/main/java/ai/rupheus/application/service/TargetService.java
@@ -1,33 +1,44 @@
 package ai.rupheus.application.service;
 
-import ai.rupheus.application.config.provider.ProviderUrlResolver;
 import ai.rupheus.application.dto.target.CreateTargetRequest;
 import ai.rupheus.application.dto.target.UpdateTargetRequest;
 import ai.rupheus.application.model.TargetModel;
 import ai.rupheus.application.model.UserModel;
 import ai.rupheus.application.model.enums.ConnectionScheme;
 import ai.rupheus.application.model.pojos.ClaudeConfig;
+import ai.rupheus.application.model.pojos.HuggingFaceConfig;
+import ai.rupheus.application.model.pojos.OpenAIConfig;
+import ai.rupheus.application.model.pojos.RestConfig;
 import ai.rupheus.application.repository.TargetRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+
+/* TODO
+* CREATE' DE CONFIG ICIN BELIRLI SCHEMELERE VALIDE OLMASI LAZIM ONUN VALIDAYSONU FIELD VALIDASYONU
+*
+
+*/
+
 
 @Service
 public class TargetService {
     private final TargetRepository targetRepository;
-    private final ProviderUrlResolver providerUrlResolver;
+    private final ObjectMapper objectMapper;
 
     @Autowired
     public TargetService(
             TargetRepository targetRepository,
-            ProviderUrlResolver providerUrlResolver
+            ObjectMapper objectMapper
     ) {
         this.targetRepository = targetRepository;
-        this.providerUrlResolver = providerUrlResolver;
+        this.objectMapper = objectMapper;
     }
 
     public TargetModel getTargetByTargetId(UUID userId, UUID targetId) {
@@ -45,6 +56,7 @@ public class TargetService {
         createdTarget.setName(createTargetRequest.getTargetName());
         createdTarget.setDescription(createTargetRequest.getTargetDescription());
         createdTarget.setScheme(connectionScheme);
+        createdTarget.setConfig(createTargetRequest.getConfig());
         createdTarget.setUser(user);
         return this.targetRepository.save(createdTarget);
     }
@@ -70,11 +82,5 @@ public class TargetService {
         return targets;
     }
 
-    private <T> T matchParameterWithScheme(Object parameters, ConnectionScheme connectionScheme) {
-        switch (connectionScheme) {
-            case CLAUDE -> new ClaudeConfig();
-        }
-
-        throw new IllegalArgumentException("Unknown connection scheme: " + connectionScheme);
-    }
+    private void ValidateParams() {}
 }
